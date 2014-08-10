83c71f8f51e1ea01d55cf2e81513faa419103883
her0e1c1 <her0e1c1@gmail.com>
2014-08-10 19:28:01 +0900

    srm628+div2med

--- srm628/Div2Medium.cpp
+++ srm628/Div2Medium.cpp
@@ -5,35 +5,102 @@
 using namespace std;
 
 #define P(x) cout << #x << " = " << (x) << endl;
+#define PS(x)                                                           \
+  cout << #x << " = ";                                                  \
+  for(char* end = &(x).top(), *begin = end - (x).size(), *i = end; i > begin ; i--) \
+    cout << *i << ", ";                                                 \
+  cout << endl;
+    
+// bool dfs(string s, stack<char>& stack){
+//   if (s.empty() && stack.empty())
+//     return true;
 
+//   bool ret = false;
 
-bool dfs(string s, vector<char>& stack){
-  for (int i = 0; i < s.size(); i++){
-    var c = s[i];
-    if (c == '[' || c == '(' || c == '{')
-      stack.push_back(c);
-    else if (c == stack.back())
+//   for (int i = 0; i < s.size(); i++){
+//     char c = s[i];
+//     string substr(s.begin() + i, s.end());
+
+//     if (c == '[' || c == '(' || c == '{')
+//       stack.push(c);
+//     else if (c == 'X'){
+//       if (!stack.empty() && (c == ']' || c == ')' || c == '}')){
+//         char c0 = stack.top();
+//         stack.pop();
+//         ret = max(dfs(substr, stack), ret);
+//         stack.push(c0);
+//       }
+//       string brankets = "({[";
+//       for (int i = 0; i < 3; i++){
+//         stack.push(brankets[i]);
+//         ret = max(dfs(substr, stack), ret);
+//         stack.pop();
+//       }
+//     } else if (c == stack.top())
+//       stack.pop();
+//   }
+//   if (stack.empty())
+//     return true;
+//   return ret;
+// }
+
+bool check(string s){
+  stack<char> stack;
+  string opens = "{([";
+  string closes = "})]";
+  for(int i = 0; i < s.size(); i++){
+    char c = s[i];
+    if (opens.find(c) != string::npos)
+      stack.push(c);
+    else if(!stack.empty() && c == closes[opens.find(stack.top())]){
       stack.pop();
-    else if (c == 'X'){
-      if (!stack.empty() && (c == ']' || c == ')' || c == '}'))
-        stack.pop();
-      
-    }
+    } else 
+      return false;
+    //PS(stack)
   }
+  if (stack.empty()) {
+    // P(s)
+  }
+  return stack.empty() ? true : false;
+}
+
+string S = "";
+string Expr;
+int N;
+bool dfs(int n){
+  //P(S)
+  string B = "{[()}]";
+  bool ret = false;
+  if (n == N)
+    return check(S);
+
+  char c = Expr[n];
+  if (c == 'X')
+    for (int i = 0; i < B.size(); i++){
+      string t(S.begin(), S.end());
+      S += B[i];
+      ret = max(dfs(n+1), ret);
+      S = t;
+    }
+  else{
+    S += c;
+    ret = max(dfs(n+1), ret);
+  }
+  return ret;
 }
 
 string run(string  expr){
-  vector<char> stack;
-  for (int i = 0; i < expr.size(); i++){
-    var c = expr[i];
-    if (c == '[' || c == '(' || c == '{')
-      stack.push_back(c);
-    else if (c == stack.back())
-      stack.pop();
-    
-  }
+  Expr = expr;
+  N = expr.length();
+  return dfs(0) ? "POSSIBLE" : "IMPOSSIBLE";
 }
 
 int main(){
-
+  P(run("([]{})"));
+  P(run("[[]]"));
+  P(run("(())[]"));
+  P(run("({])"));
+  P(run("[]X"));
+  P(run("([]X()[()]XX}[])X{{}}]"));
+  P(run("([]]()[()]]]}[])({{}}]"))
 }
