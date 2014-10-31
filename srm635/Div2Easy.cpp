e26ec1056d2f9f6a0bde4dce35cd308f5fee2607
her0e1c1 <her0e1c1@gmail.com>
2014-10-31 09:05:52 +0900

    add srm635/Div2Easy.cpp

---
+++ srm635/Div2Easy.cpp
@@ -0,0 +1,25 @@
+#include <topcoder.h>
+
+string run(string s, string t)
+{
+  int cur = 0;
+  for(int i = 0; i < s.size(); i++){
+    if (s[i] == t[cur])
+      cur++;
+  }
+  if (cur == t.size())
+    return "Yep, it's wood.";
+  else
+    return "Nope";
+}
+
+int main()
+{
+  P(run("absdefgh", "asdf"));
+  P(run("xoxox", "ooxxoo"));
+  P(run("oxoxoxox", "xxx"));
+  P(run("qwerty", "qwerty"));
+  P(run("string","longstring"));
+
+  return 0;
+}
